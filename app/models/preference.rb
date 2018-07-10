070be4b76ae9deaf288c9f51f54478425ba9e018
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-10 02:25:27 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,25 +1,25 @@
 class Preference < ActiveRecord::Base
 
-  def artist_sort_order
-  end
-  def artist_sort_order=(artist_sort_order)
-  end
+  #def artist_sort_order
+  #end
+  #def artist_sort_order=(artist_sort_order)
+  #end
 
-  def song_sort_order
-  end
-  def song_sort_order=(song_sort_order)
-  end
+  #def song_sort_order
+  #end
+  #def song_sort_order=(song_sort_order)
+  #end
 
-  def allow_create_artists
-  end
-  def allow_create_artists=(allow_create_artists)
-  end
+  #def allow_create_artists
+  #end
+  #def allow_create_artists=(allow_create_artists)
+  #end
 
 
-  def allow_create_songs
-  end
-  def allow_create_songs=(allow_create_songs)
-  end
+  #def allow_create_songs
+  #end
+  #def allow_create_songs=(allow_create_songs)
+  #end
 
 
 end
